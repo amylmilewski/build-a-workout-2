f013ab444aea72eda71cf5749cf2c6ad0f01c7a1
Amy Milewski <amy12milewski@gmail.com>
2025-08-10 11:44:32 -0400

    Adding logo to header + favicon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar () {
     return (
         <>
             <header>
-                <Link to='/' className='site-title'><h1>Build-a-Workout</h1></Link>
+                <Link to='/' className='site-title'><h1><img src="/src/assets/build_a_workout_logo.png" alt="" />Build-a-Workout</h1></Link>
                 <nav>
                 <NavLink to='/myroutines'>My Routines</NavLink>
                 <NavLink to='/myexercises'>My Exercises</NavLink>
